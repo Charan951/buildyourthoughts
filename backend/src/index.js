af211ea34287390b5b6a8a5ee605746d9e28b860
Charan951 <charanteja@speshway.com>
2026-07-20 15:04:46 +0530

    Allow Cache-Control header in CORS configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,7 +35,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cache-Control", "Pragma", "Expires"],
   })
 );
 
